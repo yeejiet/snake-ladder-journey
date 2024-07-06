06c9916c38590c5ff6ad6a13ceb3ee209c35a3ae
yeejiet <yeejiet0919@gmail.com>
2024-07-06 16:04:12 +0800

    register function

--- routes/add.js
+++ routes/add.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 module.exports = (db) => {
-    router.post('/add', (req, res) => {
+    router.post('/register', (req, res) => {
         const { username, email, password } = req.body;
         const query = 'INSERT INTO users (username, email, password) VALUES (?, ?, ?)';
         db.query(query, [username, email, password], (err, results) => {
             if (err) throw err;
-            res.redirect('/');
+            res.redirect('/login');
         });
     });
     return router;
