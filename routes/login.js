0521ba03f63b37bf9ef69354fbf23f7343b46e6c
yeejiet <yeejiet0919@gmail.com>
2024-07-16 14:17:04 +0800

    password encryption

--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,7 @@
-// login.js
+// routes/login.js
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
 
 module.exports = (db) => {
     router.get('/login', (req, res) => {
@@ -10,27 +11,37 @@ module.exports = (db) => {
         req.session.emailError = null; // Clear the error message
         req.session.passwordError = null; // Clear the error message
         req.session.email = null; // Clear the email
-        res.render('login', { emailError, passwordError, email, title:'LOGIN' });
+        res.render('login', { emailError, passwordError, email, title: 'LOGIN' });
     });
 
     router.post('/login', (req, res) => {
         const { email, password } = req.body;
-        req.session.email = email; // Store the email in session
+        req.session.email = email; 
         if (email && password) {
-            db.query('SELECT * FROM users WHERE email = ? AND password = ?', [email, password], (error, results) => {
+            db.query('SELECT * FROM users WHERE email = ?', [email], (error, results) => {
                 if (results.length > 0) {
-                    req.session.loggedin = true;
-                    req.session.email = email;
-                    req.session.id = results[0].id;
-                    req.session.username = results[0].username;
-                    req.session.score = results[0].score; // Store the score in session
-                    res.redirect('/');
+                    bcrypt.compare(password, results[0].password, (err, result) => {
+                        if (result) {
+                            req.session.loggedin = true;
+                            req.session.email = email;
+                            req.session.id = results[0].id;
+                            req.session.username = results[0].username;
+                            req.session.score = results[0].score; // Store the score in session
+                            res.redirect('/');
+                        } else {
+                            // Incorrect password
+                            req.session.passwordError = 'Incorrect Password!';
+                            res.redirect('/login');
+                        }
+                    });
                 } else {
-                    req.session.emailError = 'Incorrect Email and Password!';
+                    // No user found with given email
+                    req.session.emailError = 'Incorrect Email!';
                     res.redirect('/login');
                 }
             });
         } else {
+            // Email or password not provided
             req.session.emailError = 'Please enter Email!';
             req.session.passwordError = 'Please enter Password!';
             res.redirect('/login');
--- routes/registration.js
+++ routes/registration.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
 
 module.exports = (db) => {
     /* GET registration page. */
@@ -9,37 +10,37 @@ module.exports = (db) => {
 
     router.post('/register', (req, res) => {
         const { username, email, password } = req.body;
-
-        // Server-side validation
-        if (!username || username.length < 1) {
-            return res.render('registration', { title: 'REGISTRATION', errorMessage: 'Username must be at least 3 characters long.', field: 'username' });
-        }
-        const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        if (!email || !emailPattern.test(email)) {
-            return res.render('registration', { title: 'REGISTRATION', errorMessage: 'Please enter a valid email address.', field: 'email' });
-        }
-        const passwordPattern = /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-        if (!password || !passwordPattern.test(password)) {
-            return res.render('registration', { title: 'REGISTRATION', errorMessage: 'Password must be at least 8 characters long, include at least one number, one uppercase letter, and one special character.', field: 'password' });
-        }
-
-        // Check if username or email already exists
-        const checkUserQuery = 'SELECT * FROM users WHERE username = ? OR email = ?';
-        db.query(checkUserQuery, [username, email], (err, results) => {
-            if (err) throw err;
-
-            if (results.length > 0) {
-                return res.render('registration', { title: 'REGISTRATION', errorMessage: 'Username or email already exists.', field: 'username' });
+        bcrypt.hash(password, 10, function(err, hash) {
+            // Server-side validation
+            if (!username || username.length < 1) {
+                return res.render('registration', { title: 'REGISTRATION', errorMessage: 'Username must be at least 3 characters long.', field: 'username' });
             }
-
-            // Insert new user into database
-            const insertUserQuery = 'INSERT INTO users (username, email, password) VALUES (?, ?, ?)';
-            db.query(insertUserQuery, [username, email, password], (err, results) => {
+            const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            if (!email || !emailPattern.test(email)) {
+                return res.render('registration', { title: 'REGISTRATION', errorMessage: 'Please enter a valid email address.', field: 'email' });
+            }
+            const passwordPattern = /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+            if (!password || !passwordPattern.test(password)) {
+                return res.render('registration', { title: 'REGISTRATION', errorMessage: 'Password must be at least 8 characters long, include at least one number, one uppercase letter, and one special character.', field: 'password' });
+            }
+    
+            // Check if username or email already exists
+            const checkUserQuery = 'SELECT * FROM users WHERE username = ? OR email = ?';
+            db.query(checkUserQuery, [username, email], (err, results) => {
                 if (err) throw err;
-                res.redirect('/handbook');
+    
+                if (results.length > 0) {
+                    return res.render('registration', { title: 'REGISTRATION', errorMessage: 'Username or email already exists.', field: 'username' });
+                }
+    
+                // Insert new user into database
+                const insertUserQuery = 'INSERT INTO users (username, email, password) VALUES (?, ?, ?)';
+                db.query(insertUserQuery, [username, email, hash], (err, results) => {
+                    if (err) throw err;
+                    res.redirect('/handbook');
+                });
             });
-        });
+        })
     });
-
     return router;
 };
