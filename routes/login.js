cfd5f8bda3df3a675c45f6088ddc2f52a83173e2
yeejiet <yeejiet0919@gmail.com>
2024-08-01 16:39:53 +0800

    password bcrypt function

--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,8 @@
 // login.js
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
+
 
 module.exports = (db) => {
     router.get('/login', (req, res) => {
@@ -17,21 +19,28 @@ module.exports = (db) => {
         const { email, password } = req.body;
         req.session.email = email; // Store the email in session
         if (email && password) {
-            db.query('SELECT * FROM users WHERE email = ? AND password = ?', [email, password], (error, results) => {
+            db.query('SELECT * FROM users WHERE email = ?', [email], (error, results, fields) => {
                 if (results.length > 0) {
-                    req.session.loggedin = true;
-                    req.session.userId = results[0].id; // Ensure userId is stored in session
-                    req.session.email = email;
-                    req.session.username = results[0].username;
-                    req.session.score = results[0].score; // Store the score in session
-                    
-                    // Log the user ID and session user ID
-                    console.log('User logged in with ID:', results[0].id);
-                    console.log('Session userId:', req.session.userId);
-    
-                    res.redirect('/handbook');
+                    bcrypt.compare(password, results[0].password, function(err, result) {
+                        if (result) {
+                            req.session.loggedin = true;
+                            req.session.userId = results[0].id; // Ensure userId is stored in session
+                            req.session.email = email;
+                            req.session.username = results[0].username;
+                            req.session.score = results[0].score; // Store the score in session
+                            
+                            // Log the user ID and session user ID
+                            console.log('User logged in with ID:', results[0].id);
+                            console.log('Session userId:', req.session.userId);
+            
+                            res.redirect('/handbook');
+                        } else {
+                            req.session.passwordError = 'Incorrect Password!';
+                            res.redirect('/login');
+                        }
+                    })
                 } else {
-                    req.session.emailError = 'Incorrect Email and Password!';
+                    req.session.emailError = 'Incorrect Email!';
                     res.redirect('/login');
                 }
             });
